a6271fe8549d7ffdd991f8418242b67e8f1e686d
Terry Lee Tucker <terry@chosen-ones.org>
2021-02-12 10:32:31 -0500

    Continuing my work on the hacking course.

--- Scanning/advanced_scanner.py
+++ Scanning/advanced_scanner.py
@@ -8,9 +8,10 @@ import threading
 import ipaddress
 import optparse
 
+range_flag = False
 
 def connect_port(host, port):
-    print(f"Thread {threading.current_thread} started for {host} on {port}...")
+    # print(f"Thread {threading.current_thread} started for {host} on {port}...")
     try:
         port = int(port)
     except ValueError:
@@ -26,12 +27,13 @@ def connect_port(host, port):
         if sock.connect_ex(address) == 0:
             print(colored(f"{port}/tcp is open...", 'green'))
         else:
-            print(colored(f"{port}/tcp is closed...", 'red'))
+            pass
+            # print(colored(f"{port}/tcp is closed...", 'red'))
 
     except KeyboardInterrupt:
         print(colored("You pressed Ctrl-C. Aborting...", 'red'))
         sys.exit()
-    except socket.error:
+    except s.error:
         print(colored("Couldn't connect to server...", 'red'))
         sys.exit()
     finally:
@@ -51,13 +53,23 @@ def port_scanner(host, ports):
             print(colored(f"Cannot resolve host {host}", 'red'))
             sys.exit()
 
-    for port in ports:
-        threading.Thread(target=connect_port, args=(host, port,),
-                daemon=False).start()
+    print(colored(f"Host IP: {host_ip}", 'yellow'))
+
+    print(ports)
+    if range_flag:
+        for port in range(int(ports[0]), int(ports[1])):
+            threading.Thread(target=connect_port, args=(host, port,),
+                    daemon=False).start()
+    else:
+        for port in ports:
+            threading.Thread(target=connect_port, args=(host, port,),
+                    daemon=False).start()
 
 
 
 def main():
+    global range_flag
+
     subprocess.call('clear', shell=True)
 
     """If you use the word 'Usage' in the initialization string in
@@ -67,7 +79,7 @@ def main():
 
     parser.add_option('-H', dest = 'host', type='string', help="Target host IP") 
     parser.add_option('-p', dest='ports', type='string',
-            help="Port(s) to be scanned separated by commas") 
+            help="Port(s) to be scanned separated by commas or a range separated by a dash") 
 
     (options, args) = parser.parse_args()
     if options.host == None or options.ports == None:
@@ -75,7 +87,11 @@ def main():
         exit(0)
 
     host = options.host
-    ports = options.ports.split(',')
+    if '-' in options.ports:
+        ports = options.ports.split('-')
+        range_flag = True
+    else:
+        ports = options.ports.split(',')
 
     port_scanner(host=host, ports=ports)
 
--- Scanning/port_scan_1.py
+++ Scanning/port_scan_1.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
 
+import subprocess
+import sys
+from termcolor import colored
 import socket as s
 
-#                  IPV4      TCP
-sock = s.socket(s.AF_INET, s.SOCK_STREAM)
-host = "192.168.1.10"
-port = 449
 
-def port_scanner(port):
-    print(f"Scanning port {port}")
-    if sock.connect_ex((host, port)):
-        print(f"port {port} is closed...")
-    else:
-        print(f"port {port} is open...")
+def port_scanner(host):
+    s.setdefaulttimeout(.5)
+    try:
+        for port in range(1, 1000):
+            sock = s.socket(s.AF_INET, s.SOCK_STREAM)
+            address = (host, port)
+
+            # connect_ex() does not raise an exception on the C-level connect call but
+            # returns an error code or zero if successful.
+            if sock.connect_ex(address) == 0:
+                print(colored(f"Port {port} is open...", 'green'))
+
+            sock.close()
+    except KeyboardInterrupt:
+        print(colored("You pressed Ctrl-C. Aborting...", 'red'))
+        sys.exit()
+    except socket.error:
+        print(colored("Couldn't connect to server...", 'red'))
+        sys.exit()
+
 
 def main():
-    port_scanner(port=port)
+    subprocess.call('clear', shell=True)
+
+    host = input(colored("Enter the IP of the host to scan: ", 'yellow'))
+    # port = input(colored("Enter the port do scan: ", 'yellow'))
+    port_scanner(host)
 
 main()
