e93dfee71a5b1e62b83847b05b93fc39bbdf19e7
Terry Lee Tucker <terry@chosen-ones.org>
2021-02-24 09:17:18 -0500

    Actually, I am actually, saving my actual work. Actually...

--- password_crack/md5_pw_crack.py
+++ password_crack/md5_pw_crack.py
@@ -9,9 +9,9 @@ from termcolor import colored
 from urllib.request import urlopen
 
 
-"""Program: sha1_pw_crack.py
+"""Program: md5_pw_crack.py
 
-This program demstrates how to use a password hash value to search through a
+This program demonstrates how to use a password hash value to search through a
 very large list of common passwords, converting thime to SHA! and checking if
 the hash value you entered is found. There is some very good stuff in here.
 
@@ -24,11 +24,30 @@ def lineno():
     return inspect.currentframe().f_back.f_lineno
 
 
+def read_file(hash_str, pwd_file):
+    found = False
+    try:
+        with open(pwd_file) as file:
+            for password in file.readlines():
+                password = password.strip()
+                print(colored(f"Trying {password}", 'yellow'))
+                hash_val = hashlib.md5()    # MD5 hash object
+                hash_val.update(password.encode('utf-8')    # update hash object
+                if hash_val.hexdigest() == hash_str:
+                    print(colored(f"MD5 hash, {hash_str}, is the password, '{pwd}'",
+                            'green'))
+                    found = True
+                    break
+        return found
+    except Exception as e:
+        print(colored(f"{e}", 'red'))
+
+
 def main():
     subprocess.call('clear', shell=True)
 
-    hash_str = input(colored("Enter a SHA1 hash value: ", 'yellow'))
-    pwd_list = str(urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt').read(), 'utf-8')
+    hash_str = input(colored("Enter a MD5 hash value: ", 'yellow'))
+    pwd_file = input(colored("Enter password file name: ", 'yellow'))
 
     found = False
     for pwd in pwd_list.split():
