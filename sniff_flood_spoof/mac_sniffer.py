dcde7f4ce73bffa89e9e398226041d67f19f20b1
Terry Lee Tucker <terry@chosen-ones.org>
2021-02-27 10:50:46 -0500

    Saving my work before moving on to next section.

--- sniff_flood_spoof/mac_sniffer.py
+++ sniff_flood_spoof/mac_sniffer.py
@@ -9,12 +9,26 @@ from struct import *
 
 
 """
+35. Getting source/destination mac address from received packets: mac_sniffer.py.
+
+This program is quite a bit different than the one in the tutorial. The tutorial
+version will not run using Python3 and I don't make it a habit to use Python 2.
+
 """
 
-protocol = {6: "TCP",
-            8: "EGP",}
+proto_numbers = {
+    8: "EGP",
+    6: "TCP",
+}
+
+
+def get_proto(proto):
+    return proto_numbers.get(proto, f"<{proto}> Undefined")
+
 
 def fmt_mac(value):
+    """Takes a hex byte array and formats it as a mac address."""
+
     mac = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (
             value[0],
             value[1],
@@ -25,16 +39,13 @@ def fmt_mac(value):
     return mac
 
 
-def get_protocol(key):
-    return protocol.get(key, f"{key}: Undefined")
-
 def main():
     subprocess.call('clear', shell=True)
 
     try:
         sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
         while True:
-            packet = sock.recvfrom(1024)
+            packet = sock.recvfrom(1024)    # 1024 is buffer size, not number of ports
             packet = packet[0]
 
             # The Ether header is 14 bytes, 6 each for src and dst mac plus 2
@@ -44,10 +55,10 @@ def main():
             # This returns a tuple with three values in it: the src mac, the dst
             # mac, and the protocol.
             value = unpack('6s6sH', ether_header)
-            proto = get_protocol(value[2])
-            print(colored(f"src: {fmt_mac(value[0])}  " +
-                    f"dst: {fmt_mac(value[1])}  " + 
-                    f"proto: {proto}", 'green'))
+            proto = get_proto(value[2])
+            print(colored(f"Source MAC: {fmt_mac(value[0])}\t" +
+                    f"Destination MAC: {fmt_mac(value[1])}\t" + 
+                    f"Protocol: {proto}", 'green'))
 
     except socket.error as e:
         print(colored(f"{e}", 'red'))
@@ -60,3 +71,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
